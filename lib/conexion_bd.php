1819ea4659f32983ff824dd8866aed1374b18b12
Chelileli <elizabeth.a.gutierrez.l@gmail.com>
2014-07-05 18:53:52 -0400

    datos agregados

--- lib/conexion_bd.php
+++ lib/conexion_bd.php
@@ -1,8 +1,8 @@
 <?php
 $nombre_bd = 'GCadmin';
 $host = 'localhost';
-$usuario = 'diego';
-$password = 'rodrigo';
+$usuario = 'Cheli';
+$password = 'pepin123';
 
 try {
     $conexion_bd = new PDO("pgsql:dbname={$nombre_bd};host={$host}",
