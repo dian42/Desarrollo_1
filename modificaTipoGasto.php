c2f317d930d3b90901918fd84c303801cab62f1d
Chelileli <elizabeth.a.gutierrez.l@gmail.com>
2014-07-10 11:10:14 -0400

    modificado modificarTipoGasto

--- modificaTipoGasto.php
+++ modificaTipoGasto.php
@@ -1,14 +1,14 @@
 <?php
-include_once 'lib/Conexion_BD.php';
-
+//include_once 'lib/conexion_bd.php';
+require_once 'lib/twigLoad.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if(isset($_POST['Descripcion'])) {
 		$tga_nombre = $_POST['Descripcion'];
-		$insert_bd = $Conexion_BD -> exec("UPDATE tipo_gasto SET tga_nombre = '$tga_nombre'  WHERE tga_nombre = '$tga_nombre'");
-		$conexion_bd = NULL;
-		print_r($insert_bd);
+		//$insert_bd = $Conexion_BD -> exec("UPDATE tipo_gasto SET tga_nombre = '$tga_nombre'  WHERE tga_nombre = '$tga_nombre'");
+		//$conexion_bd = NULL;
+		//print_r($insert_bd);
 	}
 }
 else 
-	render('modificaTipoGasto.html');
+	render('modificarTipoGasto.html.twig', array('tGasto' => array('id' => 1, 'descripcion' => "inllegable")));
 ?>
