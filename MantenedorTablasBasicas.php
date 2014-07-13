4343209a9ea0864ecb4e437458ea4506b3577605
dian42 <diegonavarro42@gmail.com>
2014-07-13 14:35:47 -0400

    link mantenedor ok

--- MantenedorTablasBasicas.php
+++ MantenedorTablasBasicas.php
@@ -1,28 +1,29 @@
 <?php
 require_once 'lib/twigLoad.php';
-include_once 'lib/conexion_bd.php';
 
 session_start(); //Iniciamos una posible sesión
 
 
 if (count($_SESSION) != 0 && $_SESSION['tipo'] == false ) {
-// Se prepara la consulta y luego se ejecuta.
-$consulta_bd = $conexion_bd->prepare("SELECT * FROM tipo_gasto");
-$consulta_bd -> execute();
+	include_once 'lib/conexion_bd.php';
+	// Se prepara la consulta y luego se ejecuta.
+	$consulta_bd = $conexion_bd->prepare("SELECT * FROM tipo_gasto");
+	$consulta_bd -> execute();
 
-/* Se obtiene el primer registro del conjunto de resultados
- * como un arreglo asociativo */
-$tGasto = $consulta_bd->fetchAll(PDO::FETCH_ASSOC);
+	/* Se obtiene el primer registro del conjunto de resultados
+	 * como un arreglo asociativo */
+	$tGasto = $consulta_bd->fetchAll(PDO::FETCH_ASSOC);
 
-// Se cierra la conexión con la BD.
-$conexion_bd = NULL;
+	// Se cierra la conexión con la BD.
+	$conexion_bd = NULL;
 
 
 	render('basicos/index.html.twig', array('tGasto' => $tGasto, 'valido' => $_SESSION['valido']));
 } else {
-	$conexion_bd = NULL; // se cierra la conexión a la BD
-	//render('login/index.html.twig', array());
-	include 'login.php';
+	if ($_SESSION['tipo'] == true ) {
+		render('default/index.html.twig', array('valido' => $_SESSION['valido']));
+	}else
+		render('login/index.html.twig', array());
 }
 
 ?>
